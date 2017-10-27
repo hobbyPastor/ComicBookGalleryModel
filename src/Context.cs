ca2c176d39fbdacf9911f9a885fdf3a573b6e2f6
Kristian Berrum <kristian.berrum@live.com>
2017-10-27 21:51:00 +0200

    Rescued Version

---
+++ src/Context.cs
@@ -0,0 +1,19 @@
+﻿using ComicBookGalleryModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookGalleryModel
+{
+    public class Context : DbContext
+    {
+        public Context()
+        {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<Context>());
+        }
+        public DbSet<ComicBook> ComcicBooks { get; set; }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookGalleryModel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new Context())
+            {
+                context.ComcicBooks.Add(new Models.ComicBook()
+                {
+                    SeriesTitle = "The Amazing Spider-Man",
+                    IssueNumber = 1,
+                    PublishedOn = DateTime.Today
+                });
+                context.SaveChanges();
+
+                var comicBooks = context.ComcicBooks.ToList();
+
+                foreach (var comicBook in comicBooks)
+                {
+                    Console.WriteLine(comicBook.SeriesTitle);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
