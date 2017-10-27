57fc727bc7e493f01b1b02afbf388b2a57f4ba98
Kristian Berrum <kristian.berrum@live.com>
2017-10-28 01:39:17 +0200

    Added Role class and ComicBookArtist bridge class to map Artist with explicit roles.

--- src/Program.cs
+++ src/Program.cs
@@ -34,31 +34,38 @@ namespace ComicBookGalleryModel
                 {
                     Name = "Jack Kirby"
                 };
-
-                var comicBook1 = new Models.ComicBook()
+                var role1 = new Role()
+                {
+                    Name = "Script"
+                };
+                var role2 = new Role()
+                {
+                    Name = "Pencils"
+                };
+                var comicBook1 = new ComicBook()
                 {
                     Series = series1,
                     IssueNumber = 1,
                     PublishedOn = DateTime.Today
                 };
-                comicBook1.Artists.Add(artist1);
-                comicBook1.Artists.Add(artist2);
-                var comicBook2 = new Models.ComicBook()
+                comicBook1.AddArtist(artist1, role1);
+                comicBook1.AddArtist(artist2, role2);
+                var comicBook2 = new ComicBook()
                 {
                     Series = series1,
                     IssueNumber = 2,
                     PublishedOn = DateTime.Today
                 };
-                comicBook2.Artists.Add(artist1);
-                comicBook2.Artists.Add(artist2);
-                var comicBook3 = new Models.ComicBook()
+                comicBook2.AddArtist(artist1, role1);
+                comicBook2.AddArtist(artist2, role2);
+                var comicBook3 = new ComicBook()
                 {
                     Series = series2,
                     IssueNumber = 1,
                     PublishedOn = DateTime.Today
                 };
-                comicBook3.Artists.Add(artist1);
-                comicBook3.Artists.Add(artist3);
+                comicBook3.AddArtist(artist1, role1);
+                comicBook3.AddArtist(artist3, role2);
 
                 context.ComcicBooks.Add(comicBook1);
                 context.ComcicBooks.Add(comicBook2);
@@ -67,15 +74,17 @@ namespace ComicBookGalleryModel
 
                 var comicBooks = context.ComcicBooks
                     .Include(cb => cb.Series)
-                    .Include(cb => cb.Artists)
+                    .Include(cb => cb.Artists.Select(a => a.Artist))
+                    .Include(cb => cb.Artists.Select(a => a.Role))
                     .ToList();
 
                 foreach (var comicBook in comicBooks)
                 {
-                    var artistNames = comicBook.Artists.Select(a => a.Name).ToList();
-                    var artistsDisplayText = string.Join(", ", artistNames);
+                    var artistRoleNames = comicBook.Artists
+                        .Select(a => $"{a.Artist.Name} : {a.Role.Name}").ToList();
+                    var artistsRolesDisplayText = string.Join(", ", artistRoleNames);
                     Console.WriteLine(comicBook.DisplayText);
-                    Console.WriteLine(artistsDisplayText);
+                    Console.WriteLine(artistsRolesDisplayText);
                 }
                 Console.ReadLine();
             }
