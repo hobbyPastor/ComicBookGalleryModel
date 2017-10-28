88968f05f1e418ded6f86583e4425ff3bbbb3fab
Kristian Berrum <kristian.berrum@live.com>
2017-10-28 02:10:16 +0200

    Added override method for OnModelCreating in Context class to change Decimal propert Convention in ComicBook. Removed PluralizingTableNameConvention.

--- src/Context.cs
+++ src/Context.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,5 +16,17 @@ namespace ComicBookGalleryModel
             Database.SetInitializer(new DropCreateDatabaseAlways<Context>());
         }
         public DbSet<ComicBook> ComcicBooks { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            //modelBuilder.Conventions.Remove<DecimalPropertyConvention>();
+            //modelBuilder.Conventions.Add(new DecimalPropertyConvention(5, 2));
+
+            modelBuilder.Entity<ComicBook>()
+                .Property(cb => cb.AverageRating)
+                .HasPrecision(5, 2);
+        }
     }
 }
