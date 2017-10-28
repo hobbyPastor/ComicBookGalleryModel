e0821209dc25a31c78a41a46e71d0943e5701f52
Kristian Berrum <kristian.berrum@live.com>
2017-10-28 14:47:11 +0200

    Tested and analyzed different results with LINQ Query.

--- src/Program.cs
+++ src/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
+using System.Diagnostics;
 
 namespace ComicBookGalleryModel
 {
@@ -14,20 +15,58 @@ namespace ComicBookGalleryModel
         {
             using (var context = new Context())
             {
-                var comicBooks = context.ComicBooks
+                context.Database.Log = (message) => Debug.WriteLine(message);
+
+                //var comicBooks = context.ComicBooks.ToList();
+                //var comicBooksQuery = from cb in context.ComicBooks select cb;
+                //var comicBooks = comicBooksQuery
+                //    .Include(cb => cb.Series)
+                //    .OrderByDescending(cb => cb.IssueNumber)
+                //    .ThenBy(cb => cb.PublishedOn)
+                //    .ToList();
+
+                var comicBooksQuery = context.ComicBooks
                     .Include(cb => cb.Series)
-                    .Include(cb => cb.Artists.Select(a => a.Artist))
-                    .Include(cb => cb.Artists.Select(a => a.Role))
+                    .OrderByDescending(cb => cb.IssueNumber);
+
+                var comicBooks = comicBooksQuery.ToList();
+
+                var comicBooks2 = comicBooksQuery
+                    .Where(cb => cb.AverageRating < 7)
                     .ToList();
 
                 foreach (var comicBook in comicBooks)
                 {
-                    var artistRoleNames = comicBook.Artists
-                        .Select(a => $"{a.Artist.Name} : {a.Role.Name}").ToList();
-                    var artistsRolesDisplayText = string.Join(", ", artistRoleNames);
                     Console.WriteLine(comicBook.DisplayText);
-                    Console.WriteLine(artistsRolesDisplayText);
                 }
+                Console.WriteLine();
+                Console.WriteLine("# of comic books {0}", comicBooks.Count);
+                Console.WriteLine();
+
+                foreach (var comicBook in comicBooks2)
+                {
+                    Console.WriteLine(comicBook.DisplayText);
+                }
+                Console.WriteLine();
+                Console.WriteLine("# of comic books {0}", comicBooks2.Count);
+
+
+
+
+                //var comicBooks = context.ComicBooks
+                //    .Include(cb => cb.Series)
+                //    .Include(cb => cb.Artists.Select(a => a.Artist))
+                //    .Include(cb => cb.Artists.Select(a => a.Role))
+                //    .ToList();
+
+                //foreach (var comicBook in comicBooks)
+                //{
+                //    var artistRoleNames = comicBook.Artists
+                //        .Select(a => $"{a.Artist.Name} : {a.Role.Name}").ToList();
+                //    var artistsRolesDisplayText = string.Join(", ", artistRoleNames);
+                //    Console.WriteLine(comicBook.DisplayText);
+                //    Console.WriteLine(artistsRolesDisplayText);
+                //}
                 Console.ReadLine();
             }
         }
